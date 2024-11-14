6a4f2c1ced3f7e29b9c2580dc7bde1acebb1bdf4
nephcode <nephcohen@gmail.com>
2024-11-14 03:00:28 +0100

    contract

---
+++ contracts/People.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract People {
+
+    struct Person {
+        string name;
+        uint age; 
+    }
+
+    Person public moi;
+
+
+}
