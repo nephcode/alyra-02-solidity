19a4ebada754c4f371d3dc3000b077a46721539a
nephcode <nephcohen@gmail.com>
2024-11-14 03:44:07 +0100

    commit Array

--- contracts/People.sol
+++ contracts/People.sol
@@ -8,9 +8,14 @@ contract People {
         uint age; 
     }
 
-    Person public moi;
+    Person[] public persons;
 
-    function modifyPerson (string memory _name, uint _age ) public {
-        moi = Person(_name, _age);
+    function add(string memory _name, uint _age) public{
+       Person memory person = Person(_name, _age);
+       persons.push(person);
     }
+     
+    function remove() public{
+        persons.pop();
+    }   
 }
