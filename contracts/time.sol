5c63ea67637a19936e5b69588403c93b13bb9266
nephcode <nephcohen@gmail.com>
2024-11-25 08:52:07 +0100

    compile contract

--- contracts/time.sol
+++ contracts/time.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract time {
+contract Time {
     function getTime() public view returns (uint){
         return block.timestamp;
     }
