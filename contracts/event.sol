adf779b5ae64ef23f115ce95d8d689ea183e8d06
nephcode <nephcohen@gmail.com>
2025-01-04 08:22:56 +0100

    finished

--- contracts/event.sol
+++ contracts/event.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+// Neah game learning solidity / Did System
+// Convention _privateVariable avec un prefix underscore
+
 contract Bank {
 
     mapping(address => uint256) public balances;
     
+
+
+    // permet d'inscrire des logs sur la blockchain
     event Deposit (address indexed user, uint256 amount);
     event Withdraw (address indexed user, uint256 amount);
 
@@ -35,5 +41,11 @@ contract Bank {
     //verifier le solde
     function getBalance(address _user) external view returns(uint256) {
         return balances[_user];
+    }
+
+    // permet des action sur la VM sans gas si inscription sur la blockchain. C
+    // Permet de faire un double check pour vérifier les datas du front 
+    function addition(uint256 value_A, uint256 value_B) external pure returns(uint256){
+        return value_A + value_B;
     }
 }
