e7857cc6d1646623fe9b69ffcc9690b41b19eff6
nephcode <nephcohen@gmail.com>
2025-10-20 15:59:47 +0200

    mapping created

---
+++ contracts2025/Whitelist.sol
@@ -0,0 +1,14 @@
+/*  <!-- ∴∵ ALYRA  ƸӜƷ ∴∵ 42 ∴ +-+-+-+-+-+-+-+-+ 42 ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ ∴∵ 3--> */
+/*  <!- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ ∵ -> |N|E|P|H|C|O|D|E| <!-- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ  0--> */
+/*  <!-- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ --> +-+-+-+-+-+-+-+-+ ∵ ƸӜƷ ∴∵ ƸӜƷ 42 ƸӜƷ ∴∵  1--> */
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract Whitelist {
+
+    // classic whitelist
+    mapping (address => bool) whiteList;
+
+
+}
