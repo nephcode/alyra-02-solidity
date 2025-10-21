b0c10e0b8f37c758f268a902f3c34ad48c41afcc
nephcode <nephcohen@gmail.com>
2025-10-21 15:16:35 +0200

    Bank Correction

--- contracts2025/Bank.sol
+++ contracts2025/Bank.sol
@@ -3,24 +3,28 @@ pragma solidity 0.8.30;
 
 contract Bank
 {
-    mapping (address _address, string balance[]) _balances;
+    mapping (address => uint) balances;
 
     function deposit(uint _amount)
     public payable 
     {
-
+        balances[msg.sender]+_amount;
     }
 
-    function tranfer(address _recipient,uint _amount)
+    function transfer(address _recipient, uint _amount)
     public payable 
     {
-
+        require (_recipient != address(0), "Burn is not authorized");
+        require (balances[msg.sender]>=_amount, "Not enough Tokens");
+        balances[msg.sender]-=_amount;
+        balances[_recipient]+=_amount;
     }
 
-    fonction balanceOf(address _address)
+    function balanceOf(address _address)
     public view 
+    returns (uint)
     {
-
+        return balances[_address];
     }
 
 }
