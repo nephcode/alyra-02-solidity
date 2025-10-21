8a07fc9109e76ef8c7a95c0ac51dea449cf52219
nephcode <nephcohen@gmail.com>
2025-10-21 16:21:33 +0200

    Admin work in progress

---
+++ contracts2025/Admin.sol
@@ -0,0 +1,24 @@
+/*  <!-- ∴∵ ALYRA  ƸӜƷ ∴∵ 42 ∴ +-+-+-+-+-+-+-+-+ 42 ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ ∴∵ 3--> */
+/*  <!- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ ∵ -> |N|E|P|H|C|O|D|E| <!-- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ  0--> */
+/*  <!-- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ --> +-+-+-+-+-+-+-+-+ ∵ ƸӜƷ ∴∵ ƸӜƷ 42 ƸӜƷ ∴∵  1--> */
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
+
+import  "@openzeppelin/contracts/access/Ownable.sol"
+
+contract Admin is Ownable 
+{
+    
+
+    mapping (address => bool) isWhiteListed;
+    mapping (address => bool) isBlackListed;
+
+    constructor(onlyOwner
+    )
+
+
+
+
+
+}
