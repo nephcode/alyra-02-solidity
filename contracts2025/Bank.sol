93f3d41a8524b0b1207a39f38ddc4449987a66ab
nephcode <nephcohen@gmail.com>
2025-10-21 14:33:07 +0200

    bank Exercice 1_

---
+++ contracts2025/Bank.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract Bank
+{
+    mapping (address _address, string balance[]) _balances;
+
+    function deposit(uint _amount)
+    public payable 
+    {
+
+    }
+
+    function tranfer(address _recipient,uint _amount)
+    public payable 
+    {
+
+    }
+
+    fonction balanceOf(address _address)
+    public view 
+    {
+
+    }
+
+}
