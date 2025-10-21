49c6d7f9b16d70f1a820e083d9450ec69ea8846f
nephcode <nephcohen@gmail.com>
2025-10-21 08:53:56 +0200

    time exercice

---
+++ contracts2025/TimeContract.sol
@@ -0,0 +1,15 @@
+/*  <!-- ∴∵ ALYRA  ƸӜƷ ∴∵ 42 ∴ +-+-+-+-+-+-+-+-+ 42 ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ ∴∵ 3--> */
+/*  <!- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ ∵ -> |N|E|P|H|C|O|D|E| <!-- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ  0--> */
+/*  <!-- ƸӜƷ ∴∵ ƸӜƷ ∴∵ ƸӜƷ --> +-+-+-+-+-+-+-+-+ ∵ ƸӜƷ ∴∵ ƸӜƷ 42 ƸӜƷ ∴∵  1--> */
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract TimeContract 
+{
+    function getTime()
+    public view 
+    {
+        return (block.timestamp);
+    }
+}
